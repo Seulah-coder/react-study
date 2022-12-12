0cc4ba2dacb3dd82f25a83eda5004086d5000f9a
Seulah Kim <seulahkim@chunjae.co.kr>
2022-12-12 13:24:14 +0900

    react cloneCoding - basic, state, props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
 
+function Btn({banana, big, changeValue, fontSize = 16}) {
+    //prop.banana = {banana}
+    console.log(banana, big);
+    console.log(banana, "was rendered")
+    console.log(fontSize);
+    return <button
+        onClick={changeValue}
+        style={{
+            backgroundColor: "tomato",
+            color: "white",
+            padding: "10px 20px",
+            border: 0,
+            borderRadius: 10,
+            // fontSize: big? 18 : 16
+            fontSize: fontSize,
+        }}
+    > {banana}
+    </button>
+}
+
+//reuseable button
+
+//if the component changes the state, make it re-render
+
+//reactMemo - we can decide which one should be rendered***
+const MemorizedBtn = React.memo(Btn);
+
+//at least to get warning(nice warning for developer)
+Btn.propTypes = {
+    banana: PropTypes.string.isRequired,
+    fontSize: PropTypes.number,
+    x : PropTypes.bool
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = React.useState("Save Changes");
+    const changeValue = () => setValue("Revert Changes");
+    //함수를 btn에 argument로 보내기
+    return (<div>
+        <MemorizedBtn banana={value} changeValue={changeValue} x={false} big={true} fontSize={18}/>
+        <MemorizedBtn banana="Continue" y={7}/>
+        {/*<Btn banana={value} changeValue={changeValue} x={false} big={true}/>*/}
+        {/*<Btn banana="Continue" y={7}/>*/}
+    </div>);
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,32 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Count(){
+    const [counter, setCounter] = React.useState(0);
+    const onClick = () => {
+        // setCounter(counter + 1)
+        setCounter((current) => current+ 1);
+
+    };
+
+}
+
+
+
+// const span = React.createElement("span", {
+//     id : "test-span",
+//     onMouseEnter: () => console.log("mouse enter"),
+//     },
+//     "hello I'm a span"); //same as html element's name 1.html type 2.property 3.contents
+// const btn = React.createElement(
+//     "button",
+//     {
+//         onClick: () => console.log('im clicked'),
+//     },
+//         "Click me"
+//     );
+const button = ( <button style={{backgroundColor : "tomato",}} onClick={() => console.log("i am clicked")}>click me</button>);
 root.render(
   <React.StrictMode>
     <App />
