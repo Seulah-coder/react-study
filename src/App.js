00807cf5061ba7daeb5bba5003f52eac8852f86c
Seulah Kim <seulahkim@chunjae.co.kr>
2022-12-13 10:27:44 +0900

    cryptocurrency converter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {event.preventDefault();
-        if(toDo === ""){
-        return false;
-    }
-        setToDo("");
-        /*we cannot modify state directly (like toDo = "")*/
-        setToDos(currentArray => [toDo, ...currentArray]);
-    };
-    console.log(toDos);
-    console.log(toDos.map((item, index) => (<li key={index}>{item}</li>)));
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    const [currentPrice, setCurrentPrice] = useState(1);
+    const [amount, setAmount] = useState(1);
 
+
+    const onChange = (event) => {
+        console.log(event.target.value);
+        setCurrentPrice(event.target.value);
+        setAmount(1);
+    }
+
+    const inputAmount = (event) => {
+        console.log(event);
+        setAmount(event.target.value);
+    }
+
+    useEffect(()=> {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json)
+                setLoading(false)
+            });
+    },[])
     return (<div>
-            <h1>My To Dos ({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-             <input onChange={onChange} value={toDo} type="text" placeholder="write your to do..."/>
-                <button>Add To Do</button>
-            </form>
-            <hr />
-            <ul>
-            {toDos.map((item, index) => (<li key={index}>{item}</li>))}</ul>
+        <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
+            {loading ? <strong>Loading...</strong>: <select onChange={onChange}>
+                {coins.map((coin) => <option key={coin.id} value={coin.quotes.USD.price}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</option>)}
+            </select>}
+            <h2>Please enter the amount</h2>
+            <div>
+                <input type="number" value={amount} onChange={inputAmount} placeholder="USD"/>
+            </div>
+            <h2>you can get {amount/currentPrice}</h2>
+
     </div>
     );
 }
